lbryio/lbry-sdk

progress on publish command: py3 porting and integration tests

lbrynet/daemon/Components.py
@@ -157,6 +157,7 @@ class WalletComponent(Component):
         db = self.component_manager.get_component(DATABASE_COMPONENT)
         lbryschema.BLOCKCHAIN_NAME = conf.settings['blockchain_name']
         self.wallet = LbryWalletManager.from_lbrynet_config(conf.settings, db)
+        self.wallet.old_db = db
         yield self.wallet.start()
 
     @defer.inlineCallbacks

D474designs/lbry

lbrynet/daemon/Components.py
@@ -328,6 +328,7 @@ class WalletComponent(Component):
         storage = self.component_manager.get_component(DATABASE_COMPONENT)
         lbryschema.BLOCKCHAIN_NAME = conf.settings['blockchain_name']
         self.wallet = LbryWalletManager.from_old_config(conf.settings)
+        self.wallet.old_db = storage
         yield self.wallet.start()
 
     @defer.inlineCallbacks