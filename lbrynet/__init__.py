lbryio/lbry-sdk

linux build added and uploading to S3

lbrynet/__init__.py
@@ -1,6 +1,6 @@
 import logging
 
-__version__ = "0.21.0rc2"
+__version__ = "0.30.0alpha"
 version = tuple(__version__.split('.'))
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())

D474designs/lbry

lbrynet/__init__.py
@@ -1,6 +1,6 @@
 import logging
 
-__version__ = "0.21.0rc4"
+__version__ = "0.30.0a"
 version = tuple(__version__.split('.'))
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())